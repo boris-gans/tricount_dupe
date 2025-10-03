55719dd5cf8d770849a504a72c7c66b97746d4f3
boris-gans <borisgans418@gmail.com>
2025-10-03 11:09:58 +0200

    --

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,8 +13,8 @@ class Settings(BaseSettings):
     base_logger_name: str
 
     @property
-    def database_url(self): #to build url from env
-        return f"postgresql+psycopg2://{self.database_user}:{self.database_pw}@localhost:5432/{self.database_name}"
+    def database_url(self):
+        return f"postgresql+psycopg2://{self.database_user}:{self.database_pw}@db:5432/{self.database_name}"
 
     class Config: #tell pydantic to load variables from .env
         env_file = ".env"
